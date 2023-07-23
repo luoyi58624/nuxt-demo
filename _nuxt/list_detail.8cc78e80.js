c3b59a24658ab811960676f737d9dc8b2197f7ee
luoyi58624 <46178521+luoyi58624@users.noreply.github.com>
2023-07-23 09:35:24 +0000

    Deploying to pages from @ luoyi58624/nuxt-demo@5bf6c615bb45ddd313d1273c646646a4c43e83da 🚀

--- _nuxt/list_detail.8cc78e80.js
+++
@@ -1,1 +0,0 @@
-import{g as e,o as t,c as o,a as s}from"./entry.2ad33118.js";const a=s("h1",null,"列表页详情页",-1),n=[a],i=e({__name:"list_detail",setup(_){return(c,l)=>(t(),o("div",null,n))}});export{i as default};
---
+++ static/about.b0248aac.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./entry.8442dea1.js";const o={};function a(n,r){return c(),t("p",null,"this is about page")}const _=e(o,[["render",a]]);export{_ as default};
