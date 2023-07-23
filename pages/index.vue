a41994bda35024705037eec92d7248bdcda2ce9e
luoyi <luoyi58624@gmail.com>
2023-07-23 18:11:44 +0800

    xx

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,6 @@
   <div>
     <h1>首页</h1>
     <button class="btn" @click="count++">count: {{ count }}</button>
-    <!-- <v-btn depressed class="m-1" @click="count++">count: {{ count }}</v-btn>
-    <v-btn color="primary" class="m-1" @click="count++">count: {{ count }}</v-btn> -->
     <ul>
       <li v-for="item in data['data']">{{ item['appName'] }}</li>
     </ul>
