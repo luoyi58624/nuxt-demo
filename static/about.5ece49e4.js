cc34f4dc3b6ea66956e4a2af1a0ae55dfb1248a4
luoyi58624 <46178521+luoyi58624@users.noreply.github.com>
2023-07-23 12:48:30 +0000

    Deploying to pages from @ luoyi58624/nuxt-demo@5cf3de657ce4d3e94ae1cb0619b89e54b39e0043 🚀

--- static/about.5ece49e4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c}from"./entry.43e5ca50.js";const o={};function a(n,r){return c(),t("p",null,"this is about page")}const _=e(o,[["render",a]]);export{_ as default};
--- static/my.07aea47a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./entry.43e5ca50.js";const o={};function r(t,a){return n(),c("h1",null,"My Page")}const _=e(o,[["render",r]]);export{_ as default};
